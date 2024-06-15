1c0b509240fde08be0dae31b6c867e7ab2375a4b
Nessim Yohros <nessim@MacBook-Pro-109.local>
2024-06-15 16:40:37 -0400

    added boilerplate code

--- server/app.js
+++ server/app.js
@@ -1,14 +1,59 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+// Set security HTTP headers
+app.use(helmet());
+
+// Enable CORS
+app.use(cors());
+app.options('*', cors());
+
+// Logger
 app.use(morgan('dev'));
 
+// Compress responses
+app.use(compression());
+
+// Body parsers
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// Rate limiting
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Custom error handling
 app.all('*', (req, res, next) => {
-  console.log('an error occurred');
+  console.log('An error occurred');
 });
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
@@ -8,6 +9,20 @@ process.on('uncaughtException', (err) => {
 });
 
 dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
