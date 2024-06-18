a29a90505eb1ac882055ca376c2f7bb8ea0ef50f
Nessim Yohros <nessim@MacBook-Pro-109.local>
2024-06-18 17:30:10 -0400

    added logout function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -86,4 +86,10 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.logout = catchAsync(async (req, res, next) => {});
+exports.logout = catchAsync(async (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: 'success' });
+});
