3858c8acf1fd126cebf0ad739158ae251f02c22e
Nessim Yohros <nessim@MacBook-Pro-109.local>
2024-07-08 15:25:32 -0400

    added some fixes to css styling

--- client/src/features/messaging/ChannelListContainer.jsx
+++ client/src/features/messaging/ChannelListContainer.jsx
@@ -1,14 +1,28 @@
 import { ChannelList, useChatContext } from 'stream-chat-react';
-import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+// import {
+//   ChannelSearch,
+//   TeamChannelList,
+//   TeamChannelPreview,
+// } from 'stream-chat-react';
 import Cookies from 'js-cookie';
+import '../../App.css';
 
-function SideBar() {
-  <div className="channel-list__sidebar">
-    <div className="channel-list__sidebar__icon1"></div>
-  </div>;
+function MessageHeader() {
+  return (
+    <div className="channel-list__header">
+      <p className="channel-list__header__text">Your Group Channels</p>
+    </div>
+  );
 }
+
 function ChannelListContainer() {
-  return <div>ChannelListContainer</div>;
+  return (
+    <>
+      <div className="channel-list__list__wrapper">
+        <MessageHeader />
+      </div>
+    </>
+  );
 }
 
 export default ChannelListContainer;
